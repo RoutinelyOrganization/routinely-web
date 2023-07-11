f89037b41047eb595a23c76b3f22b5b2d24e2c20
marinsgui <marins0guilherme@gmail.com>
2023-07-11 14:25:20 -0300

    feat: created global styles

---
+++ src/GlobalStyles.tsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Roboto', sans-serif;
+    }
+`;
