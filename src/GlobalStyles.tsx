3d77f47b910573d415022fced7b5ef268b38643b
Luis Soares <luissoaress.dev@gmail.com>
2023-07-13 06:42:02 -0300

    feat: home page created

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');
     * {
         margin: 0;
         padding: 0;
---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,35 @@
+import { ButtonComponent } from "../../components/Button/ButtonStyles";
+import Logo from "../../components/Logo";
+import {
+  ButtonHomePageComponent,
+  CaptionHomePageComponent,
+  HeaderHomePageComponent,
+  MainHomePageComponent,
+  TitleHomePageComponent,
+} from "./HomePageStyles";
+import homePageImage from "../../assets/imagens/homePageImage.svg";
+
+export default function HomePage() {
+  return (
+    <>
+      <HeaderHomePageComponent>
+        <Logo />
+        <ButtonComponent children="Recursos" />
+        <ButtonComponent children="Planos" />
+        <ButtonComponent children="Acesse" $secondaryColor />
+      </HeaderHomePageComponent>
+
+      <MainHomePageComponent>
+        <TitleHomePageComponent>
+          Domine sua rotina e conquiste o dia com nossa ferramenta Routinely.
+        </TitleHomePageComponent>
+        <CaptionHomePageComponent>
+          Simplifique sua vida, alcance seus objetivos e encontre equilíbrio com nossa ferramenta de organização de
+          rotina pessoal.
+        </CaptionHomePageComponent>
+        <ButtonHomePageComponent>Teste de graça</ButtonHomePageComponent>
+        <img src={homePageImage} alt="imagem inicial da home page" />
+      </MainHomePageComponent>
+    </>
+  );
+}
