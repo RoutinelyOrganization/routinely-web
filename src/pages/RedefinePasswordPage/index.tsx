5afe253e80b364a1241ad073fb51530cbf3e6404
marinsgui <marins0guilherme@gmail.com>
2023-07-17 17:29:11 -0300

    feat: finished styling on redefinePassword page

--- src/pages/RedefinePasswordPage/index.tsx
+++ src/pages/RedefinePasswordPage/index.tsx
@@ -1,6 +1,5 @@
 import ButtonComponent from "../../components/Button";
 import HeaderComponent from "../../components/Header";
-import InputComponent from "../../components/Input";
 import LinkAuthComponent from "../../components/LinkAuth";
 import LogoSharedComponent from "../../components/LogoShared";
 import SubTitleAuthComponent from "../../components/SubTitleAuth";
@@ -8,6 +7,7 @@ import TitleAuthComponent from "../../components/TitleAuth";
 import { ForgotPasswordImageContainer } from "../ForgotPasswordPage/ForgotPasswordPageStyles";
 import { RedefinePasswordPageStyle, RedefinePasswordPageWrapper } from "./RedefinePasswordPageStyle";
 import forgotPasswordImage from "../../assets/imagens/forgotPasswordImage.svg";
+import { InputStyle } from "../../components/Input/InputStyles";
 
 export default function RedefinePasswordPage() {
   return (
@@ -18,11 +18,13 @@ export default function RedefinePasswordPage() {
           <LogoSharedComponent />
           <TitleAuthComponent>Redefinir senha</TitleAuthComponent>
           <SubTitleAuthComponent>Insira o código de verificação enviado no email.</SubTitleAuthComponent>
-          <InputComponent type="text" placeholder="Código de verificação" />
-          <LinkAuthComponent path="#" linkText="Enviar Novamente">
-            Não recebeu?
-          </LinkAuthComponent>
-          <ButtonComponent>Confirmar</ButtonComponent>
+          <form>
+            <InputStyle type="text" placeholder="Código de verificação" />
+            <LinkAuthComponent path="#" linkText="Enviar Novamente">
+              Não recebeu?
+            </LinkAuthComponent>
+            <ButtonComponent>Confirmar</ButtonComponent>
+          </form>
         </RedefinePasswordPageWrapper>
         <ForgotPasswordImageContainer>
           <img src={forgotPasswordImage} alt="Imagem da página redefinir senha" />
