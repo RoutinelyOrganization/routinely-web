63d915d7cc63a08994beffbcd9dd412ef19ffe33
Luis Soares <luissoaress.dev@gmail.com>
2023-07-20 22:39:12 -0300

    feat: welcome page created

---
+++ src/pages/WelcomePage/index.tsx
@@ -0,0 +1,34 @@
+import HeaderComponent from "../../components/Header";
+import LogoSharedComponent from "../../components/LogoShared";
+import {
+  ButtonAccount,
+  ButtonCreateAccount,
+  CaptionWelcomePage,
+  ContainerButtonWelcomePage,
+  TitleWelcomePage,
+  WelcomePageWrapper,
+} from "./WelcomePageStyles";
+import welcomePageImage from "../../assets/imagens/welcomePageImage.svg";
+import { useNavigate } from "react-router-dom";
+
+export function WelcomePage() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <HeaderComponent />
+      <WelcomePageWrapper>
+        <form>
+          <LogoSharedComponent />
+          <TitleWelcomePage>Boas-vindas</TitleWelcomePage>
+          <CaptionWelcomePage>Escolha uma das opções para acessar </CaptionWelcomePage>
+          <ContainerButtonWelcomePage>
+            <ButtonAccount onClick={() => navigate("/SignInPage")}>Já tenho conta</ButtonAccount>
+            <ButtonCreateAccount onClick={() => navigate("/SignUpPage")}>Criar conta</ButtonCreateAccount>
+          </ContainerButtonWelcomePage>
+        </form>
+        <img src={welcomePageImage} alt="Imagem da página de boas vindas" />
+      </WelcomePageWrapper>
+    </>
+  );
+}
