f9e64407e6bcde141ed840fd0431879f55e3ad83
Luis Soares <luissoaress.dev@gmail.com>
2023-09-20 18:53:08 -0300

    feat: added axios instance file

---
+++ src/services/api.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://routinely-api-dev.onrender.com/",
+});
+export default instance;
+
+
+// requições feitas por aqui com base no exemplo do Discord feito pela Day.
