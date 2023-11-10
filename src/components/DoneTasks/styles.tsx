0f41cfb317f81dc2d268b5867e1225b35f782029
staviasz <erickesc21@hotmail.com>
2023-11-10 12:21:19 -0300

    refactor: Componente DoneTasks

--- src/components/DoneTasks/styles.tsx
+++ src/components/DoneTasks/styles.tsx
@@ -1,6 +1,6 @@
 import { styled } from "styled-components";
 
-export const DoneTasksStyle = styled.div`
+export const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
