8cff19e71d4994a550af154848f77442a8353e3c
staviasz <erickesc21@hotmail.com>
2023-11-14 11:19:25 -0300

    fix: resolving style warnings

--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -1,10 +1,14 @@
 import styled from "styled-components";
-import { IHeader } from ".";
+
+interface IHeader {
+  $header?: 'primary' | 'secundary'
+}
+
 
 export const Header = styled.header<IHeader>`
   display: flex;
   justify-content: space-between;
-  padding: ${props => props.header === 'primary'?'.92rem 6.7rem;':'32px 120px;'};
+  padding: ${props => props.$header === 'primary'?'.92rem 6.7rem;':'32px 120px;'};
   align-items: center;
   gap: 80px;
   background-color: #5c59bb;
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,7 +3,7 @@ import ErrorMessage from "../ErrorMessage";
 import * as S from "./styles";
 
 interface IInput {
-  children?: React.JSX.Element;
+  children?: React.ReactNode;
   label?: string;
   type?: string;
   placeholder?: string;
@@ -12,14 +12,15 @@ interface IInput {
   id?: string;
   required?: boolean;
   errorMessage?: string;
+  autoComplete?: string
 
 }
 
-export default function InputComponent({ label,type, placeholder, register, hasError, id, required, children, errorMessage}: IInput) {
+export default function InputComponent({ label,type, placeholder, register, hasError, id, required, children, errorMessage,autoComplete}: IInput) {
   return (
     <>
       <S.InputContainer>
-        <S.InputStyle type={type} placeholder={placeholder} {...register} $hasErro={hasError} id={id} required={required} />
+        <S.InputStyle type={type} placeholder={placeholder} {...register} $hasErro={hasError} id={id} required={required} autoComplete={autoComplete} />
         <S.LabelInput $hasErro={hasError} htmlFor={id}>{label}</S.LabelInput>
         {children}
       </S.InputContainer>
