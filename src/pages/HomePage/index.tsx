52cd6f14c2e7f84601c65722a2cb54f42d464508
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-04-30 01:45:35 -0300

    feat(HomePage): remove test for free button

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -52,7 +52,6 @@ export default function HomePage() {
               Simplifique sua vida, alcance seus objetivos e encontre equilíbrio com nossa ferramenta de organização de
               rotina pessoal.
             </S.Caption>
-            <S.Button>Teste de graça</S.Button>
           </div>
           <img className="desktop" src={homePageBanner} alt="imagem inicial da home page" />
           <img className="tablet-horizontal" src={completedTasks} alt="tarefas concluídas" />
